998ca02c0979967af8676bbc870e4cb47bc3b7cf
osparking <jbpark03@gmail.com>
2024-10-28 19:41:15 +0900

    input w/ onChange handler, submit button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
 export default function Todo() {
-    return <div>
-        Todo
+  const [todo, setTodo] = useState("");
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">등록</button>
+      </form>
     </div>
+  );
 }
