24e0d0d6ae37b4690fde773ad1413a8458d71911
osparking <jbpark03@gmail.com>
2024-10-29 08:45:15 +0900

    prevent default cleaning of input box

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  function handleTodo(e) {
+    e.preventDefault();
+    // Todo를 API로 전달하거나 스토리지에 저장하세요
+    // setTodo("");
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleTodo}>
         <input
           type="text"
           onChange={(e) => setTodo(e.target.value)}
