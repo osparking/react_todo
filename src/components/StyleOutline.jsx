47588b29ebcb4d3c48bce6525d3362805c92c1d5
osparking <jbpark03@gmail.com>
2024-10-29 22:19:37 +0900

    css module definition and usage

--- src/components/StyleOutline.jsx
+++ src/components/StyleOutline.jsx
@@ -1,9 +1,9 @@
-import "../css/style.css";
+import styles from "./StyleOutline.module.css";
 
 export default function StyleOutline() {
   return (
     <div>
-      <h1 className="header-outline">Style - Outline method</h1>
+      <h1 className={styles.header}>Style - Outline method</h1>
     </div>
   );
 }
