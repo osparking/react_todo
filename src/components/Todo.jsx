afd76286fca405866f827fb181548118892d09d8
osparking <jbpark03@gmail.com>
2024-10-29 09:10:43 +0900

    add todo item into todoList - 1st try

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
   function handleTodo(e) {
     e.preventDefault();
-    // Todo를 API로 전달하거나 스토리지에 저장하세요
-    // setTodo("");
+    setTodoList([todo]);
+    setTodo("");
+    console.log(todoList);
   }
 
   return (
