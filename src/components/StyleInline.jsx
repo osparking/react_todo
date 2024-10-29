55b926d6ff2507b7e7e74f1fb81a7a5ee30da019
osparking <jbpark03@gmail.com>
2024-10-29 21:44:50 +0900

    define two header classes and use them

--- src/components/StyleInline.jsx
+++ src/components/StyleInline.jsx
@@ -3,7 +3,7 @@ import "../css/style.css";
 export default function StyleInline() {
   return (
     <div>
-      <h1 className="header">Style - Inline method</h1>
+      <h1 className="header-inline">Style - Inline method</h1>
     </div>
   );
 }
