986c3f76b3a6dd820f908527ea863e27005fd74c
osparking <jbpark03@gmail.com>
2024-10-30 20:08:38 +0900

    style register button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,13 +9,16 @@ export default function Form({ todoList, setTodoList }) {
   }
   return (
     <form className={styles.formtodo} onSubmit={handleTodo}>
-      <input className={styles.input_modern}
+      <input
+        className={styles.input_modern}
         type="text"
         onChange={(e) => setTodo(e.target.value)}
         value={todo}
         placeholder="여기에 할 일을 입력..."
       />
-      <button type="submit">등록</button>
+      <button className={styles.button_modern} type="submit">
+        등록
+      </button>
     </form>
   );
 }
