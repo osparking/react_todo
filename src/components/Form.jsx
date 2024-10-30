8a0602910cde7ced10775428bce0933a09cce9b9
osparking <jbpark03@gmail.com>
2024-10-30 20:25:36 +0900

    place input box and register button in one line

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,16 +9,18 @@ export default function Form({ todoList, setTodoList }) {
   }
   return (
     <form className={styles.formtodo} onSubmit={handleTodo}>
-      <input
-        className={styles.input_modern}
-        type="text"
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-        placeholder="여기에 할 일을 입력..."
-      />
-      <button className={styles.button_modern} type="submit">
-        등록
-      </button>
+      <div className={styles.container_input}>
+        <input
+          className={styles.input_modern}
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          placeholder="여기에 할 일을 입력..."
+        />
+        <button className={styles.button_modern} type="submit">
+          등록
+        </button>
+      </div>
     </form>
   );
 }
