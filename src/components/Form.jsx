c96c712754555da4ed658a4f661b6b9cd88fba4a
osparking <jbpark03@gmail.com>
2024-10-30 19:04:14 +0900

    add placeholder text for todo input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,10 +9,11 @@ export default function Form({ todoList, setTodoList }) {
   }
   return (
     <form className={styles.formtodo} onSubmit={handleTodo}>
-      <input
+      <input className={styles.input_modern}
         type="text"
         onChange={(e) => setTodo(e.target.value)}
         value={todo}
+        placeholder="여기에 할 일을 입력..."
       />
       <button type="submit">등록</button>
     </form>
