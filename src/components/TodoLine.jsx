7801391de32b38ece3f439abc36d0e5b4733fe78
osparking <jbpark03@gmail.com>
2024-10-31 12:26:31 +0900

    button span after inner div

--- src/components/TodoLine.jsx
+++ src/components/TodoLine.jsx
@@ -1,8 +1,11 @@
 import styles from "./todoline.module.css";
 export default function TodoLine({ todo }) {
   return (
-    <div className={styles.todoLine}>
+    <div className={styles.todoLine} >
       <div className={styles.todoName}>{todo}</div>
+      <span>
+        <button>X</button>
+      </span>
       <hr className={styles.hLine} />
     </div>
   );
