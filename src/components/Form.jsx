f328b962ecd96631f82458cad171e0a5a7b6c5e2
osparking <jbpark03@gmail.com>
2024-10-31 19:04:14 +0900

    make todo a class object w/ 2 fields

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "./form.module.css";
 export default function Form({ todoList, setTodoList }) {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState({subject: "", done: false});
   function handleTodo(e) {
     e.preventDefault();
     setTodoList([...todoList, todo]);
