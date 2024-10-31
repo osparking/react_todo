9fcba9e95bad8c6e86a0d1621b469c777c195138
osparking <jbpark03@gmail.com>
2024-10-31 12:31:02 +0900

    move button span right after {todo} string

--- src/components/TodoLine.jsx
+++ src/components/TodoLine.jsx
@@ -1,11 +1,13 @@
 import styles from "./todoline.module.css";
 export default function TodoLine({ todo }) {
   return (
-    <div className={styles.todoLine} >
-      <div className={styles.todoName}>{todo}</div>
-      <span>
-        <button>X</button>
-      </span>
+    <div className={styles.todoLine}>
+      <div className={styles.todoName}>
+        {todo}
+        <span>
+          <button>X</button>
+        </span>
+      </div>
       <hr className={styles.hLine} />
     </div>
   );
