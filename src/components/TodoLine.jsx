76ede97dfc37a1d7063f5cf9f35054b2d0ad59d3
osparking <jbpark03@gmail.com>
2024-10-31 14:52:29 +0900

    handle delete button click - 1st cut

--- src/components/TodoLine.jsx
+++ src/components/TodoLine.jsx
@@ -1,11 +1,16 @@
 import styles from "./todoline.module.css";
 export default function TodoLine({ todo }) {
+  function deleteTodo() {
+    console.log("할일 삭제 시도됨");
+  }
   return (
     <div className={styles.todoLine}>
       <div className={styles.todoName}>
         {todo}
         <span>
-          <button className={styles.buttonDelete}>x</button>
+          <button onClick={deleteTodo} className={styles.buttonDelete}>
+            x
+          </button>
         </span>
       </div>
       <hr className={styles.hLine} />
