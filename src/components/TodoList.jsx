c1c7bc586b18fb64cfdbeb1d1dcbca38dcfff59f
osparking <jbpark03@gmail.com>
2024-10-31 15:45:44 +0900

    send two list related params to TodoLine

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,15 @@
 import TodoLine from "./TodoLine";
 import styles from "./todolist.module.css";
-export default function TodoList({ todoList }) {
+export default function TodoList({ todoList, setTodoList }) {
   return (
     <div className={styles.list}>
       {todoList.map((todo) => (
-        <TodoLine todo={todo} key={todo} />
+        <TodoLine
+          todo={todo}
+          key={todo}
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
       ))}
     </div>
   );
