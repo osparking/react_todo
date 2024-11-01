7052ce0ad9c7521395eb816990ec9315e6c40266
osparking <jbpark03@gmail.com>
2024-11-01 14:45:58 +0900

    use item class for footer text span

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
-import styles from './footer.module.css'
+import styles from "./footer.module.css";
 
-export default function Footer({completedCount}) {
-    return <div className={styles.footer}>
-        <span>완료된 일 : {completedCount} 건</span>
+export default function Footer({ completedCount }) {
+  return (
+    <div className={styles.footer}>
+      <span className={styles.item}>완료된 일 : {completedCount} 건</span>
     </div>
+  );
 }
