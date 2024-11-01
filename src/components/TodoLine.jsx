70c4275e6219897179e548c7ddb390febe34741f
osparking <jbpark03@gmail.com>
2024-11-01 11:00:32 +0900

    span class changes on todo.done field

--- src/components/TodoLine.jsx
+++ src/components/TodoLine.jsx
@@ -3,6 +3,7 @@ export default function TodoLine({ todo, todoList, setTodoList }) {
   function deleteTodo(todo) {
     setTodoList(todoList.filter((item) => item !== todo));
   }
+
   function subjectClicked(subject) {
     setTodoList(
       todoList.map((item) =>
@@ -12,10 +13,17 @@ export default function TodoLine({ todo, todoList, setTodoList }) {
     console.log(todoList);
   }
 
+  const ifCompleted = todo.done ? styles.taskCompleted : "";
+
   return (
     <div className={styles.todoLine}>
       <div className={styles.todoName}>
-        <span onClick={() => subjectClicked(todo.subject)}>{todo.subject}</span>
+        <span
+          className={ifCompleted}
+          onClick={() => subjectClicked(todo.subject)}
+        >
+          {todo.subject}
+        </span>
         <span>
           <button
             onClick={() => deleteTodo(todo)}
