9602a7b0e016782ea241d7178ae9c0677d37bcde
osparking <jbpark03@gmail.com>
2024-11-01 12:09:33 +0900

    calculate completed todos and display it

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,10 +4,12 @@ import TodoList from "./TodoList";
 
 export default function Todo() {
   const [todoList, setTodoList] = useState([]);
+  const completedCount = todoList.filter((todo) => todo.done).length;
   return (
     <div>
       <Form todoList={todoList} setTodoList={setTodoList} />
       <TodoList todoList={todoList} setTodoList={setTodoList} />
+      <h1>{completedCount}</h1>
     </div>
   );
 }
