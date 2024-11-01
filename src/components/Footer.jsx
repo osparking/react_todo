403af5be48a8c3b3e051c061b8c3e4c58e25c098
osparking <jbpark03@gmail.com>
2024-11-01 14:54:40 +0900

    total number also displayed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import styles from "./footer.module.css";
 
-export default function Footer({ completedCount }) {
+export default function Footer({ completedCount, totalCount }) {
   return (
     <div className={styles.footer}>
       <span className={styles.item}>완료된 일 : {completedCount} 건</span>
+      <span className={styles.item}>전체 일 : {totalCount} 건</span>
     </div>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,11 +6,12 @@ import TodoList from "./TodoList";
 export default function Todo() {
   const [todoList, setTodoList] = useState([]);
   const completedCount = todoList.filter((todo) => todo.done).length;
+  const totalCount = todoList.length;
   return (
     <div>
       <Form todoList={todoList} setTodoList={setTodoList} />
       <TodoList todoList={todoList} setTodoList={setTodoList} />
-      <Footer completedCount={completedCount} />
+      <Footer completedCount={completedCount} totalCount={totalCount} />
     </div>
   );
 }
